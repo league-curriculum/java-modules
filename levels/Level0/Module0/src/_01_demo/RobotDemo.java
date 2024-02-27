ca8071385899d51ec7c370fdee82c2fa8d605c0e
Eric Busboom <eric@civicknowledge.com>
2024-02-27 08:51:07 -0800

    .

--- levels/Level0/Module0/src/_01_demo/RobotDemo.java
+++ levels/Level0/Module0/src/_01_demo/RobotDemo.java
@@ -6,12 +6,14 @@ package _01_demo;
  */
 
 import org.jointheleague.graphical.robot.Robot;
+import java.awt.Color;
+import java.sql.Driver;
 
 // This recipe draws a square using the Robot
 
 public class RobotDemo {
 
-	public static void main(String[] args) {
+	public static void demo0() {
 
 		// This code makes a new Robot
 		Robot rob = new Robot();
@@ -26,6 +28,41 @@ public class RobotDemo {
 			rob.move(200);
 			rob.turn(70);
 		}
+	}
 
+	public static void demo1() {
+		Robot rob = new Robot();
+		String url = "http://icons.iconarchive.com/icons/martin-berube/character/96/Robot-icon.png";
+		rob.changeRobot(url);
+		rob.setSpeed(10);
+		rob.penDown();
+	}
+
+	 public static void demo2() {
+		Robot rob = new Robot(125, 125);
+		rob.setSpeed(10);
+		rob.miniaturize();
+		rob.penDown();
+		rob.setPenColor(Color.RED);
+		rob.sleep(1000);
+		int[] moves = new int[]{260, 150, 260, 150};
+  
+		for(int i = 0; i < moves.length; ++i) {
+		   rob.turn(90);
+		   rob.move(moves[i]);
+		}
+  
+		rob.turn(90);
+		rob.penUp();
+		rob.move(185);
+		rob.turn(-90);
+		rob.setSpeed(2);
+		rob.move(-75);
+	 }
+
+
+
+	public static void main(String[] args) {
+		demo2();
 	}
 }
